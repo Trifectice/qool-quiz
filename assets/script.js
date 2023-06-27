4a489308c373d2b551e65af4875f56182d42c726
Scott Keller <scottkeller@Scotts-MacBook-Pro.local>
2023-06-27 16:41:15 -0600

    Adding event listeners , checking question repsonses and ending quiz

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,22 @@
 //Questions
-
+//Questions
+var questions = [
+    {
+      question: "What programming language is used to style a webpage?",
+      choices: ["JavaScript", "PineScript", "C++", "CSS"],
+      answer: "CSS"
+    },
+    {
+      question: "What is the starting point in an array?",
+      choices: ["0", "1", "A", "0.0"],
+      answer: "0"
+    },
+    {
+      question: "Which programming language is used for web development?",
+      choices: ["Java", "Python", "JavaScript", "C++"],
+      answer: "JavaScript"
+    }
+  ];
 //Global Vars
 var currentQuestionIndex = 0;
 var time = 60;
@@ -13,7 +30,7 @@ var resultElement = document.getElementById("result");
 var initialsInput = document.getElementById("initials");
 var scoreElement = document.getElementById("score");
 var saveButton = document.getElementById("save");
-//Event listeners
+
 //Start function
 function startQuiz() {
     startButton.disabled = true;
@@ -30,8 +47,44 @@ function startQuiz() {
       }
     }, 1000);
   }
+
+  function showQuestion() {
+    var question = questions[currentQuestionIndex];
+    questionElement.textContent = question.question;
+    choicesElement.innerHTML = "";
+  
+    question.choices.forEach(function(choice) {
+      var choiceButton = document.createElement("button");
+      choiceButton.textContent = choice;
+      choiceButton.classList.add("choice");
+      choiceButton.addEventListener("click", checkAnswer);
+      choicesElement.appendChild(choiceButton);
+    });
+}    
+//Event listeners
+startButton.addEventListener("click", startQuiz);
+saveButton.addEventListener("click", saveScore);
 //Check question
-//End quiz
+function checkAnswer(event) {
+  var selectedChoice = event.target;
+  var answer = questions[currentQuestionIndex].answer;
+  //Correct or wrong response
+  if (selectedChoice.textContent === answer) {
+    resultElement.textContent = "Correct!";
+    correctAnswers++;
+  } else {
+    resultElement.textContent = "Wrong!";
+    time -= 10;
+  }
+
+  currentQuestionIndex++;
+  //End quiz
+  if (currentQuestionIndex < questions.length) {
+    showQuestion();
+  } else {
+    endQuiz();
+  }
+}
 //Timer stop 
 //Score calculation
 //Save intials and save score to Local
